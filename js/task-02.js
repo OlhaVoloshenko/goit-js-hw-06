5ba0ababfd09d1cfc00b9a959788949b9ad40ba7
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-04-13 21:12:55 +0300

    HW #1
    HW #1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const navItemEl = document.createElement('li');
+
+navItemEl.classList.add('item');
+
+console.log(navItemEl);
+const navLinkEl = document.createElement('a');
+navLinkEl.classList.add('item__link');
+navLinkEl.textContent = '';
+navLinkEl.href = '#ingredients';
+
+navItemEl.appendChild(navLinkEl);
+console.log(navItemEl);
+
+const navEl = document.querySelector('#ingredients');
+navEl.appendChild(navItemEl);

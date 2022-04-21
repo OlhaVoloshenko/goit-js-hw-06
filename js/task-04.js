1cad787e0d8a4a00f8360f4adeb9adb5bcbda2b9
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-04-21 16:13:45 +0300

    HW #4
    HW #4

--- js/task-04.js
+++ js/task-04.js
@@ -1,8 +1,10 @@
-const battonConteynerEl = document.querySelector('#counter');
-const decrementBtn = battonConteynerEl.firstElementChild;
-const incrementBtn = battonConteynerEl.lastElementChild;
+//we sort out the array
+const buttonConteynerEl = document.querySelector('#counter');
+//we create variable items
+const dctBtn = buttonConteynerEl.firstElementChild;
+const ictBtn = buttonConteynerEl.lastElementChild;
 const valueEl = document.querySelector('#value');
-
+//we create variable and initialize its meaning
 const counterValue = 0;
 
 const counter = {
@@ -16,12 +18,14 @@ const counter = {
      this.value += 1; 
    },
 }
+const dctBtnLeft = () => {
+   counter.decrement();
+   valueEl.textContent = counter.value;
+};
+const dctbtnRigth = () => {
+   counter.increment();
+   valueEl.textContent = counter.value;
+};
 
-decrementBtn.addEventListener('click', function () {
-counter.decrement();
-valueEl.textContent = counter.value;
-});
-incrementBtn.addEventListener('click', function () {
-counter.increment();
-valueEl.textContent = counter.value;
-});
+dctBtn.addEventListener('click', dctBtnLeft);
+ictBtn.addEventListener('click', dctbtnRigth);

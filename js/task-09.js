59ce78143eab0449bfbef13a5b03375f9ce30754
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-04-25 10:23:14 +0300

    HW #9
    HW #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,14 @@
-const buttonEl = document.querySelector('.change-color'),
-textColorEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+const textColorEl = document.querySelector('.color');
 
 buttonEl.addEventListener("click", () => {
-  console.log("Button was clicked");
   document.body.style.backgroundColor = getRandomHexColor();
   textColorEl.textContent = getRandomHexColor();
 })
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 console.log(getRandomHexColor());

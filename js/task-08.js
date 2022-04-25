87fa0364aee31c47d91f79d3dd911940f83fef7e
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-04-25 10:12:15 +0300

    HW #8
    HW #8

--- js/task-08.js
+++ js/task-08.js
@@ -3,21 +3,19 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
    event.preventDefault();
-  //  document.forms.form.reset();
-   const formElements = event.currentTarget.elements;
+
+   const formEl = event.currentTarget.elements;
  
-  if (formElements.email.value === "" || formElements.password.value === "") {
+  if (formEl.email.value === "" || formEl.password.value === "") {
    return alert("Будь ласка заповніть усі поля");
  }
- const email = formElements.email.value;
- const password = formElements.password.value;
+ const email = formEl.email.value;
+ const password = formEl.password.value;
   const formData = {
    email, password, 
   }
   console.log(formData);
-  formElements.email.value = '';
-  formElements.password.value = '';
-  // form.defaultValue();
-  // formElements.getElementById('myform').reset();
+  formEl.email.value = '';
+  formEl.password.value = '';
 };
 
